a237ad59c368c80994cdbf3110c8cea2d79152ad
Trofim Petyanov <trofimpetyanov@icloud.com>
2022-06-18 16:20:23 +0400

    Xcode project initialized

---
+++ Block 1 – SimbirSoft Internship/Block 1 – SimbirSoft Internship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Block 1 – SimbirSoft Internship
+//
+//  Created by Trofim Petyanov on 18.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
