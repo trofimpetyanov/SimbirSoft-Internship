cbe9088cfe7b2d848e27860ec842498818721b05
Trofim Petyanov <trofimpetyanov@icloud.com>
2022-06-28 18:08:49 +0400

    DataManager created

---
+++ Block 1 – SimbirSoft Internship/Block 1 – SimbirSoft Internship/DataManager.swift
@@ -0,0 +1,14 @@
+//
+//  DataManager.swift
+//  Block 1 – SimbirSoft Internship
+//
+//  Created by Trofim Petyanov on 28.06.2022.
+//
+
+import Foundation
+
+struct DataManager {
+    static let shared = DataManager()
+    
+    private init() { }
+}
