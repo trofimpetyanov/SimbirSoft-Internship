a81f3bcfbefb2551192098c327cf0994329c6b83
Trofim Petyanov <trofimpetyanov@icloud.com>
2022-07-11 22:37:45 +0400

    NavigationBar stylized

--- Block 1 – SimbirSoft Internship/Block 1 – SimbirSoft Internship/Service/AppearanceConfigurator.swift
+++ Block 1 – SimbirSoft Internship/Block 1 – SimbirSoft Internship/Service/AppearanceConfigurator.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 enum AppearanceConfigurator {
-    static func configure() {
-        let appearance = UINavigationBar.appearance()
-        appearance.backgroundColor = UIColor.greenColor
-        appearance.tintColor = .white
-        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+    static func configureNavigationBar() {
+        let titleTextAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont(name: "OfficinaSansExtraBoldSCC", size: 21)!
+        ]
+        
+        let buttonTextAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont(name: "OfficinaSansExtraBoldSCC", size: 18)!
+        ]
+    
+        UINavigationBar.appearance().tintColor = UIColor.white
+        
+        if #available(iOS 13.0, *) {
+            let barButtonItemApperanace = UIBarButtonItemAppearance()
+            barButtonItemApperanace.normal.titleTextAttributes = buttonTextAttributes
+            
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+            navigationBarAppearance.backgroundColor = UIColor.greenColor
+            navigationBarAppearance.titleTextAttributes = titleTextAttributes
+            navigationBarAppearance.buttonAppearance = barButtonItemApperanace
+            
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = UINavigationBar.appearance().standardAppearance
+        } else {
+            UINavigationBar.appearance().barTintColor = UIColor.greenColor
+            UINavigationBar.appearance().titleTextAttributes = titleTextAttributes
+            UIBarButtonItem.appearance().setTitleTextAttributes(buttonTextAttributes, for: .normal)
+        }
     }
 }
